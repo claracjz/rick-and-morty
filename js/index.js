df726670a52210e3d32902f1b41be9598492036e
Ana Vitória <anacvitoria00@gmail.com>
2025-05-07 18:32:00 -0300

    feat: pagination, footer and new styles

--- js/index.js
+++ js/index.js
@@ -2,11 +2,30 @@ document.addEventListener("DOMContentLoaded", main);
 
 async function main() {
 
-    const result = await listCharactersByPage();
-
-    renderCharactersList(result.charactersList);
+    loadMainContent(1);
+    renderFooterContent();
     
 }
+
+async function loadMainContent(page) {
+    const result = await listCharactersByPage(page);
+
+    const characters = [...result.charactersList];
+
+    for (const character of characters) {
+        const lastEpisodeUrl = character.episode[character.episode.length - 1];
+
+        const episodeName = await getEpisodeDataFromURL(lastEpisodeUrl);
+
+        character.episode = {
+            url: lastEpisodeUrl,
+            name: episodeName,
+        }
+    }
+
+    renderCharactersList(characters);
+    renderPagination(result.prevPage, result.nextPage);
+};
 
 function renderCharactersList(characters) {
     const row = document.getElementById("list-characters");
@@ -14,7 +33,7 @@ function renderCharactersList(characters) {
 
     for (const character of characters) {
         const card = `
-         <div class="card mb-3">
+         <div class="card mb-3 character-card">
                         <div class="row g-0">
                           <div class="col-12 col-md-5">
                             <div class="object-fit-fill border rounded h-100">
@@ -34,12 +53,12 @@ function renderCharactersList(characters) {
                               </p>
 
                               <p class="card-text">
-                                <small class="text-body-secondary">Última localização conhecida:</small><br>
-                                <small>Planeta XYZ</small>
+                                <small class="text-secondary">Última localização conhecida:</small><br>
+                                <small>${character.location.name}</small>
                             </p>
                             <p class="card-text">
-                                <small class="text-body-secondary">Visto a última vez em:</small><br>
-                                <small>Nome do episódio</small>
+                                <small class="text-secondary">Visto a última vez em:</small><br>
+                                <small>${character.episode.name}</small>
                             </p>
                             </div>
                           </div>
@@ -54,6 +73,77 @@ function renderCharactersList(characters) {
         col.innerHTML = card;
         row.appendChild(col);
     }
+}
+
+async function renderFooterContent() {
+    const totalCharacters = await getTotalByFeature("character");
+    const totalLocations = await getTotalByFeature("location");
+    const totalEpisodes = await getTotalByFeature("episode");
+
+
+    const spanTotalCharacters = document.getElementById("total-characters");
+    spanTotalCharacters.innerText = totalCharacters;
+
+    const spanTotalLocations = document.getElementById("total-locations");
+    spanTotalLocations.innerText = totalLocations;
+
+    const spanTotalEpisodes = document.getElementById("total-episodes");
+    spanTotalEpisodes.innerText = totalEpisodes;
+
+    const spanDevName = document.getElementById("dev-name");
+    spanDevName.innerText = "Ana Vitória";
+
+    const spanCurrentYear = document.getElementById("current-year");
+    spanCurrentYear.innerText = new Date().getFullYear()
+ 
+};
+
+function renderPagination(prevPage, nextPage) {
+const prevPageNumber = !prevPage ? 0 : prevPage.split("?page=")[1];
+const nextPageNumber = !nextPage ? 0 : nextPage.split("?page=")[1];
+
+
+      const nav = document.getElementById("pagination");
+      nav.innerHTML = "";
+      const ul = document.createElement("ul");
+      ul.classList.add("pagination", "justify-content-center");
+      
+      const liPrevPage = document.createElement("li");
+      liPrevPage.classList.add("page-item");
+
+      if(!prevPage) {
+        liPrevPage.classList.add("disabled");
+      }
+
+      const buttonPrev = document.createElement("button")
+      buttonPrev.setAttribute("type", "button");
+      buttonPrev.classList.add("page-link");
+      buttonPrev.innerText = "Prev";
+      buttonPrev.addEventListener("click", () => loadMainContent(prevPageNumber));
+
+      liPrevPage.appendChild(buttonPrev);
+
+      const liNextPage = document.createElement("li");
+      liNextPage.classList.add("page-item");
+
+      if(!nextPage) {
+        liNextPage.classList.add("disabled");
+      }
+
+      const buttonNext = document.createElement("button")
+      buttonNext.setAttribute("type", "button");
+      buttonNext.classList.add("page-link");
+      buttonNext.innerText = "Next";
+      buttonNext.addEventListener("click", () => loadMainContent(nextPageNumber));
+
+      liNextPage.appendChild(buttonNext);
+
+      ul.appendChild(liPrevPage);
+      ul.appendChild(liNextPage);
+
+      nav.appendChild(ul);
+
+
 }
 
 function mapCharacterStatus(characterStatus) {
